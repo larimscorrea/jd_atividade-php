1bc6dff4517fc422fd75fc0b02bafba63988cc94
Larissa Corrêa <101229830+larimscorrea@users.noreply.github.com>
2023-07-06 08:41:42 -0300

    Add files via upload

---
+++ one-question.php
@@ -0,0 +1,10 @@
+<?php
+// Array com cinco ou mais itens
+$arrayItens = array("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6");
+
+// Exibindo o conteúdo do array linha a linha
+foreach ($arrayItens as $item) {
+    echo $item . "<br>";
+}
+?>
+
